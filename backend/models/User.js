6cb7bf97a89b891231770f53c33f70ad28ba62ef
Rathva Roshan <151353212+ROSHANRTW@users.noreply.github.com>
2025-07-19 19:22:17 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  country:  { type: String, default: "" },
-  gender:   { type: String, default: "Male" }
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
